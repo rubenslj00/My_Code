74f69da0f484042e2794f994b38a097293936e63
schakka <31333599+chakkas@users.noreply.github.com>
2020-01-18 12:18:35 -0800

    Update update.sql

--- deploy/update.sql
+++ deploy/update.sql
@@ -1,8 +1,3 @@
 USE WAREHOUSE &warehouse;
-<<<<<<< HEAD
   
 update FLIPR.SQITCH.CHANGES set PROJECT='Marketing1' where SCRIPT_HASH='2e5a587a4d35f3460e622403bbce9d6c3a0e70f0'
-=======
-   
-update flipr.sqitch.changes set PROJECT='Marketing1' where SCRIPT_HASH='2e5a587a4d35f3460e622403bbce9d6c3a0e70f0'
->>>>>>> 35b6179af1cf40c51ff5a0af97ad67a56600fbbb

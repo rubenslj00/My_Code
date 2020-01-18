1641153b687df9cc83bad758a791a0dec3862ea7
Sharath Chakka <sharathchakka@Sharaths-MacBook-Pro.local>
2020-01-18 12:30:12 -0800

    changes

--- deploy/appschema.sql
+++ deploy/appschema.sql
@@ -1,8 +1,4 @@
 -- Deploy flipr:appschema to snowflake
 
 USE WAREHOUSE &warehouse;
-<<<<<<< HEAD
-CREATE SCHEMA chakkas;
-=======
 CREATE SCHEMA chandra;
->>>>>>> 35b6179af1cf40c51ff5a0af97ad67a56600fbbb

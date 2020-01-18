780edaabbf0416e84f254cb29fecfee5086ab693
schakka <31333599+chakkas@users.noreply.github.com>
2020-01-18 03:28:56 -0800

    Update update.sql

--- deploy/update.sql
+++ deploy/update.sql
@@ -1,5 +1,5 @@
 USE WAREHOUSE &warehouse;
-CREATE TABLE flipr.mytable (
+CREATE TABLE flipr.sqitch.mytable (
     nickname    TEXT         NOT NULL REFERENCES flipr.users(nickname),
     name        TEXT         NOT NULL,
     description TEXT         NOT NULL,
@@ -9,8 +9,8 @@ CREATE TABLE flipr.mytable (
 );
 
 
-insert into flipr.mytable
+insert into flipr.sqitch.mytable
   select 'PETER', 'JEFF', '123-456',  to_date('2020-01-16T23:39:20.123'),to_date('2019-12-31T23:39:20.123'), to_timestamp('2018-12-08T23:39:20.123');
   
   
-update FLIPR.SQITCH.CHANGES set PROJECT='Marketing1' where SCRIPT_HASH='2e5a587a4d35f3460e622403bbce9d6c3a0e70f0'
+update flipr.sqitch.changes set PROJECT='Marketing1' where SCRIPT_HASH='2e5a587a4d35f3460e622403bbce9d6c3a0e70f0'
